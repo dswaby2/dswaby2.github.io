7b361dbb02995294882c4f93c0f3e1cb25f04347
dswaby2 <63844308+dswaby2@users.noreply.github.com>
2020-04-17 04:36:40 -0400

    First Draft

---
+++ script.js
@@ -0,0 +1,50 @@
+/** Add any JavaScript you need to this file. */
+
+
+        //open silde bar
+        function openSlideMenu(){
+          document.getElementById('side-menu').style.width = '400px';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+         //close silde bar
+         function closeSlideMenu(){
+          document.getElementById('side-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '0';
+        }
+        //functions to open different side bar categories
+        function laptops(){
+          document.getElementById('laptops-menu').style.width = '400px';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function phones(){
+          document.getElementById('phones-menu').style.width = '400px';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function tabs(){
+          document.getElementById('tabs-menu').style.width = '400px';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function heads(){
+          document.getElementById('heads-menu').style.width = '400px';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+           
+       //To back back buttons for sidebar categories
+        function Lbackbutton(){
+          document.getElementById('laptops-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function Pbackbutton(){
+          document.getElementById('phones-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function Tbackbutton(){
+          document.getElementById('tabs-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+        function Hbackbutton(){
+          document.getElementById('heads-menu').style.width = '0';
+          document.getElementById('main').style.marginLeft = '250px';
+        }
+
+      
